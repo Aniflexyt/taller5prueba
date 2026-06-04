d1decd0f7f636655d7b081145ddc249f0aaf7c06
Aniflexyt <162089552+Aniflexyt@users.noreply.github.com>
2026-06-03 19:06:57 -0500

    Update logica.php

--- controlador/logica.php
+++ controlador/logica.php
@@ -1,31 +1,3 @@
-<?php
-
-//var_dump($_POST);
-
-$conexion = new PDO('pgsql:host=dpg-d8g5vu28qa3s73asc3dg-a.oregon-postgres.render.com;dbname=bd_profe','bd_profe_user','biTM7L5wwdh8SyCZGqEbVreql9d55ZZT');
-$registrar = $conexion->prepare("INSERT INTO form_sugerencias (nombre,telefono,detalles) VALUES (?, ?, ?)");
-$registrar->execute([$_POST["nom"], $_POST["tel"], $_POST["det"]]);
-echo "<p style='color:white;background-color:green;font-family:calibri,arial;font-size:24px;text-align:center'>Registro exitoso</p>";
-
-$consulta = $conexion->prepare("SELECT * FROM form_sugerencias order by id");
-$consulta->execute();
-$tabla = $consulta->fetchAll(PDO::FETCH_ASSOC);	      //PDO::FETCH_NUM
-$conexion = null;
-
-echo "<table><tr><th>Codigo</th>
-                 <th>Nombre completo</th>
-                 <th>Contacto</th>
-			     <th>Detalles</th>		</tr>";
-foreach($tabla as $fila){		//Recorre el arreglo $tabla como FETCH_NUM
-    echo "<tr>		<td>$fila[id]</td>
-            		<td>$fila[nombre]</td>
-            		<td>$fila[telefono]</td>
-            		<td>$fila[detalles]</td>		</tr>";
-}
-echo "</table>";
-
-?>
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -119,8 +91,8 @@ tr:hover{
     font-size:20px;
 }
 </style>
-</head>
 
+</head>
 <body>
 
 <div class="contenedor">
@@ -133,11 +105,46 @@ tr:hover{
         </button>
     </div>
 
+<?php
+
+//var_dump($_POST);
+
+$conexion = new PDO('pgsql:host=dpg-d8g5vu28qa3s73asc3dg-a.oregon-postgres.render.com;dbname=bd_profe','bd_profe_user','biTM7L5wwdh8SyCZGqEbVreql9d55ZZT');
+
+$registrar = $conexion->prepare("INSERT INTO form_sugerencias (nombre,telefono,detalles) VALUES (?, ?, ?)");
+$registrar->execute([$_POST["nom"], $_POST["tel"], $_POST["det"]]);
+
+echo "<p class='exito'>Registro exitoso</p>";
+
+$consulta = $conexion->prepare("SELECT * FROM form_sugerencias order by id");
+$consulta->execute();
+
+$tabla = $consulta->fetchAll(PDO::FETCH_ASSOC);
+
+$conexion = null;
+
+echo "<table>
+<tr>
+<th>Codigo</th>
+<th>Nombre completo</th>
+<th>Contacto</th>
+<th>Detalles</th>
+</tr>";
+
+foreach($tabla as $fila){
+    echo "<tr>
+            <td>$fila[id]</td>
+            <td>$fila[nombre]</td>
+            <td>$fila[telefono]</td>
+            <td>$fila[detalles]</td>
+          </tr>";
+}
+
+echo "</table>";
+
+?>
 
 </div>
-
-</body>
-</html>
 
 </body>
 </html>
