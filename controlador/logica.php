e8cc855afb96cf028d148d58d753795ff9109f5f
Aniflexyt <162089552+Aniflexyt@users.noreply.github.com>
2026-06-03 19:24:36 -0500

    Update logica.php

--- controlador/logica.php
+++ controlador/logica.php
@@ -111,25 +111,35 @@ tr:hover{
 
 $conexion = new PDO('pgsql:host=dpg-d8g5vu28qa3s73asc3dg-a.oregon-postgres.render.com;dbname=bd_profe','bd_profe_user','biTM7L5wwdh8SyCZGqEbVreql9d55ZZT');
 
-$registrar = $conexion->prepare("INSERT INTO form_sugerencias (nombre,telefono,detalles) VALUES (?, ?, ?)");
-$registrar->execute([$_POST["nom"], $_POST["tel"], $_POST["det"]]);
+/* =========================
+   SOLO SE INSERTA SI VIENE DEL FORMULARIO
+   ========================= */
+if(isset($_POST["nom"], $_POST["tel"], $_POST["det"])) {
 
-echo "<p class='exito'>Registro exitoso</p>";
+$registrar = $conexion->prepare("INSERT INTO form_sugerencias (nombre,telefono,detalles) VALUES (?, ?, ?)");
+$registrar->execute([
+    $_POST["nom"],
+    $_POST["tel"],
+    $_POST["det"]
+]);
+
+echo "<p style='color:white;background-color:green;font-family:calibri,arial;font-size:24px;text-align:center'>Registro exitoso</p>";
+
+}
+
+/* =========================
+   CONSULTA SIEMPRE
+   ========================= */
 
 $consulta = $conexion->prepare("SELECT * FROM form_sugerencias order by id");
 $consulta->execute();
-
 $tabla = $consulta->fetchAll(PDO::FETCH_ASSOC);
-
 $conexion = null;
 
-echo "<table>
-<tr>
-<th>Codigo</th>
-<th>Nombre completo</th>
-<th>Contacto</th>
-<th>Detalles</th>
-</tr>";
+echo "<table><tr><th>Codigo</th>
+                 <th>Nombre completo</th>
+                 <th>Contacto</th>
+                 <th>Detalles</th></tr>";
 
 foreach($tabla as $fila){
     echo "<tr>
@@ -143,7 +153,6 @@ foreach($tabla as $fila){
 echo "</table>";
 
 ?>
-
 </div>
 
 </body>
